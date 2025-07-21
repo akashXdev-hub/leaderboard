499215cb704c46e5d792827cacdff437db0ff6e9
akash <akashgautam7137@gmail.com>
2025-07-21 17:19:17 +0530

    Add server package.json

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const History = require('../models/History');
+const History = require('../models/history');
 const multer = require('multer');
 const path = require('path');
 
